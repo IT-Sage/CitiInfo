07a884e325e6b30d7817738b981b21247138d01b
neon333 <ryska.v@gmail.com>
2018-08-17 14:30:10 +0200

    custom controllers added city Dto added

---
+++ CitiInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitiInfo.API.Controllers
+{
+    [Route("api/cities")] //[Route("api/[controller]")]
+    public class CitiesController : Controller 
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            //var temp =  new JsonResult(CitiesDataStore.Current.Cities);
+            //temp.StatusCode = 200;
+            //return temp;
+
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);                
+        }
+    }
+}
--- CitiInfo.API/Startup.cs
+++ CitiInfo.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CitiInfo.API
 {
@@ -15,11 +16,14 @@ namespace CitiInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // writes to the console of web browser
+            loggerFactory.AddConsole();
             if (env.IsDevelopment())
             {
                 // adds the developer exeption page middleware to the request pipeline
@@ -30,10 +34,15 @@ namespace CitiInfo.API
                 app.UseExceptionHandler();
             }
 
-            app.Run((context) =>
-            {
-                throw new Exception("Example exception");
-            });
+            //shows custom page instead of blank
+            app.UseStatusCodePages();
+
+            app.UseMvc();
+
+            //app.Run((context) =>
+            //{
+            //    throw new Exception("Example exception");
+            //});
 
             //app.Run(async (context) =>
             //{
