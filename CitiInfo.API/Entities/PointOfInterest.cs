fbb6ac1e957e9cfe1e93c296749d075e3c77250c
neon333 <ryska.v@gmail.com>
2018-08-21 10:54:29 +0200

    Migration Updates added

--- CitiInfo.API/Entities/PointOfInterest.cs
+++ CitiInfo.API/Entities/PointOfInterest.cs
@@ -12,6 +12,10 @@ namespace CitiInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+                
+        [MaxLength(200)]
+        public string Description { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
